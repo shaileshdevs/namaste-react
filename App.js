40565b9882e6eef1c96451a995c87a10ec0e8bb8
shaileshdevs <shaileshdevs@gmail.com>
2024-03-03 18:36:27 +0530

    Episode 3

--- App.js
+++ App.js
@@ -1,8 +1,36 @@
-const heading = React.createElement(
-    "h1",
-    { id: "heading", xyz: "abc" },
-    "HelloWorld from React!"
-);
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-root.render(heading);
+// const heading = React.createElement(
+//     "h1",
+//     { id: "heading", class: "heading" },
+//     "Namaste React"
+// );
+
+const jsxHeading = (
+    <h1 className="heading" tabIndex="5">
+        Namaste React using JSX
+    </h1>
+);
+
+const Title = () => {
+    return (
+        <h1>
+            Namaste React Title Component
+        </h1>
+    );
+}
+
+const HeadingComponent = () => {
+    return (
+        <div id="container">
+            <Title></Title>
+            <h1 className="heading">
+                Namaste React Functional Component
+            </h1>
+        </div>
+    );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent></HeadingComponent>);
